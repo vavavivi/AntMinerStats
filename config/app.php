150606c3d9c7923d844e7726c641e9d578a2891c
hippie <v.v.gura@gmail.com>
2017-07-08 15:29:35 +0300

    InfyOm generator

--- config/app.php
+++ config/app.php
@@ -167,10 +167,15 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+	    Collective\Html\HtmlServiceProvider::class,
+	    Laracasts\Flash\FlashServiceProvider::class,
+	    Prettus\Repository\Providers\RepositoryServiceProvider::class,
+	    \InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
+	    \InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class,
 
-        /*
-         * Application Service Providers...
-         */
+	    /*
+		 * Application Service Providers...
+		 */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
@@ -225,6 +230,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
 
     ],
 
