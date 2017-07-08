a48cde20e54b110eab020ecc0ec61384b91bd3ec
hippie <v.v.gura@gmail.com>
2017-07-08 15:23:38 +0300

    fix route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
 	Route::get('/', 'HomeController@index')->name('home');
-	Route::get('/home', 'HomeController@index')->name('home');
+	Route::get('/home', function () {
+		return redirect('/');
+	});
 
 });
 
