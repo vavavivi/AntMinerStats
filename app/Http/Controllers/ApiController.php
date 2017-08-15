db38f5113d29e5cc7a0a230b97977c2a52e73fe9
hippie <v.v.gura@gmail.com>
2017-08-16 00:39:11 +0300

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -17,56 +17,25 @@ class ApiController extends Controller
 
     public function webhook(Request $request)
     {
-
-		$msg = json_encode($request->all());
+	    $i = Telegram::getMe();
 
 	    $response = Telegram::sendMessage([
 		    'chat_id' => 2421164,
-		    'text' => $msg
+		    'text' => $i
 	    ]);
 
     }
 
     public function sendInfo()
     {
-	    $a = '';
-
-
-
-	    foreach($antMiners as $antMiner)
-	    {
-		    $a .= '<b>'.$antMiner->title.'</b>:
-';
-		    $a .= 'Hashrate: <i>'.$data[$antMiner->id]['hash_rate'].' Ghs</i>
-';
-		    $a .= 'AVG Temp: <i>'.$data[$antMiner->id]['temp_avg'].' Ghs</i>
-';
-		    $a .= 'Chips OK: <i>'.$data[$antMiner->id]['chips']['ok'].'</i>
-';
-		    $a .= 'Chips Failed: <i>'.$data[$antMiner->id]['chips']['er'].'</i>
-';
-		    $a .= 'FAN1: <i>'.$data[$antMiner->id]['fans'][0].' rpm</i>
-';
-		    $a .= 'FAN2: <i>'.$data[$antMiner->id]['fans'][1].' rpm</i>
-';
-		    $a .= '
-';
-
-	    }
-
-
-
-	    $telegram->sendMessage([
+	    Telegram::sendMessage([
 		    'chat_id' => 2421164,
-		    'text' => $a,
+		    'text' => 123,
 		    'parse_mode' =>'HTML'
 	    ]);
 
 
-	    $telegram->sendMessage([
-		    'chat_id' => 81184145,
-		    'text' => $a,
-		    'parse_mode' =>'HTML'
-	    ]);
+
+
     }
 }
