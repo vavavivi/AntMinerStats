0f3ed3aa5df908bf407010e085f555f172a7d465
torbik (Alexander Dzuba) <a.y.dzuba@gmail.com>
2017-08-20 13:22:25 +0300

    Menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,17 +1,16 @@
-<li class="{{ Request::is('antMiners*') ? 'active' : '' }}">
-    <a href="{!! route('antMiners.index') !!}"><i class="fa fa-desktop"></i> <span>AntMINERS Overview</span></a>
+<li class="{{ Route::is('antMiners.index') ? 'active' : '' }}">
+    <a href="{!! route('antMiners.index') !!}"><i class="fa fa-desktop"></i> <span>Dashboard</span></a>
 </li>
 
-<li class="treeview active">
+<li class="treeview {{ Route::is('antMiners.show') ? 'active' : '' }}">
     <a href="#">
         <i class="fa fa-folder"></i>
-        <span>List of Miners</span>
+        <span>AntMiners</span>
         <span class="pull-right"><i class="fa fa-angle-down pull-right"></i></span>
     </a>
     <ul class="treeview-menu">
-
         @foreach(Auth::user()->miners as $antMiner)
-            <li class="treeview {{ Request::is('antMiners.index') ? 'active' : '' }}">
+            <li class="treeview">
                 <a href="{!! route('antMiners.show', $antMiner->id) !!}"><i class="fa fa-cube"></i> {{$antMiner->title}}</a>
             </li>
         @endforeach
