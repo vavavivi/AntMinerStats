922fc57f279261a39f6d0589da7117bd4193aaf4
hippie <v.v.gura@gmail.com>
2017-08-27 01:12:48 +0300

    fix

--- resources/views/ant_miners/table.blade.php
+++ resources/views/ant_miners/table.blade.php
@@ -30,63 +30,63 @@
 
                     @if($data[$antMiner->id])
                         <!-- HASHRATE -->
-                            <td class="text-center">
-                                <a class='btn btn-{{ $data[$antMiner->id]['hash_rate'] > $antMiner->hr_limit ? 'success' : 'warning'}} btn-xs pull-left'>{!! round(intval($data[$antMiner->id]['hash_rate'])/1000,2) !!}</a>
-                            </td>
+                        <td class="text-center">
+                            <a class='btn btn-{{ $data[$antMiner->id]['hash_rate'] > $antMiner->hr_limit ? 'success' : 'warning'}} btn-xs pull-left'>{!! round(intval($data[$antMiner->id]['hash_rate'])/1000,2) !!}</a>
+                        </td>
+                        <!-- HW ERR -->
+                        <td>
+                            <a class='btn btn-{{$data[$antMiner->id]['hw'] < 0.001 ? 'success' : 'warning'}} btn-xs pull-left'>{!! $data[$antMiner->id]['hw'] !!} %</a>
+                        </td>
 
-                            <td>
-                                <a class='btn btn-{{$data[$antMiner->id]['hw'] < 0.001 ? 'success' : 'warning'}} btn-xs pull-left'>{!! $data[$antMiner->id]['hw'] !!} %</a>
-                            </td>
-
-                            <!-- TEMP -->
-                            <td class="text-left small" nowrap="">
-                                @foreach($data[$antMiner->id]['chains'] as $chain_index => $chain_data)
-                                    @if($antMiner->type == 'bmminer')
-                                        <div class="btn-group">
-                                            <a class='btn btn-{{ intval($chain_data['brd_temp1']) < $antMiner->temp_limit ? 'success' : 'warning'}} btn-xs miner-temp'>
-                                                {{$chain_data['brd_temp1']}}°
-                                            </a>
-                                            <a class='btn btn-{{ intval($chain_data['brd_temp2']) < $antMiner->temp_limit ? 'success' : 'warning'}} btn-xs miner-temp'>
-                                                {{$chain_data['brd_temp2']}}°
-                                            </a>
-                                        </div>
-                                    @else
-                                        <a class='btn btn-{{ intval($chain_data['brd_temp']) < $antMiner->temp_limit ? 'success' : 'warning'}} btn-xs miner-temp'>
-                                            {{$chain_data['brd_temp']}}°
-                                        </a>
-                                    @endif
-                                @endforeach
-                            </td>
-
-                            <!-- Chips -->
-                            <td class="text-left small" nowrap>
-                                @foreach($data[$antMiner->id]['chains'] as $chain_index => $chain_data)
+                        <!-- TEMP -->
+                        <td class="text-left small" nowrap="">
+                            @foreach($data[$antMiner->id]['chains'] as $chain_index => $chain_data)
+                                @if($antMiner->type == 'bmminer')
                                     <div class="btn-group">
-                                        <a class="btn btn-success btn-xs chip-status">{{$chain_data['chips_condition']['ok']}}</a>
-                                        <a class="btn btn-{{ $chain_data['chips_condition']['er'] <= 0 ? 'default' : 'danger'}} btn-xs chip-status">{{$chain_data['chips_condition']['er']}}</a>
+                                        <a class='btn btn-{{ intval($chain_data['brd_temp1']) < $antMiner->temp_limit ? 'success' : 'warning'}} btn-xs miner-temp'>
+                                            {{$chain_data['brd_temp1']}}°
+                                        </a>
+                                        <a class='btn btn-{{ intval($chain_data['brd_temp2']) < $antMiner->temp_limit ? 'success' : 'warning'}} btn-xs miner-temp'>
+                                            {{$chain_data['brd_temp2']}}°
+                                        </a>
+                                    </div>
+                                @else
+                                    <a class='btn btn-{{ intval($chain_data['brd_temp']) < $antMiner->temp_limit ? 'success' : 'warning'}} btn-xs miner-temp'>
+                                        {{$chain_data['brd_temp']}}°
+                                    </a>
+                                @endif
+                            @endforeach
+                        </td>
+
+                        <!-- Chips -->
+                        <td class="text-left small" nowrap>
+                            @foreach($data[$antMiner->id]['chains'] as $chain_index => $chain_data)
+                                <div class="btn-group">
+                                    <a class="btn btn-success btn-xs chip-status">{{$chain_data['chips_condition']['ok']}}</a>
+                                    <a class="btn btn-{{ $chain_data['chips_condition']['er'] <= 0 ? 'default' : 'danger'}} btn-xs chip-status">{{$chain_data['chips_condition']['er']}}</a>
+                                </div>
+                            @endforeach
+                        </td>
+
+                        <!--Board Freq -->
+                        <td class="text-left" nowrap>
+                            @if($data[$antMiner->id])
+                                @foreach($data[$antMiner->id]['chains'] as $chain_index => $chain_data)
+                                    <a class="btn btn-default btn-xs freq">B{{$loop->index + 1}}: {{intval($chain_data['brd_freq'])}}</a>
+                                    <div class="progress vertical progress-xxs">
+                                        <div class="progress-bar progress-bar-info progress-bar-striped"
+                                             role="progressbar"
+                                             aria-valuenow="{{intval($chain_data['brd_freq'])}}"
+                                             aria-valuemin="100" aria-valuemax="1000"
+                                             style="height: {{round(intval($chain_data['brd_freq']) * 100 / (1000 - 100))}}%">
+                                        </div>
                                     </div>
                                 @endforeach
-                            </td>
-
-                            <!--Board Freq -->
-                            <td class="text-left" nowrap>
-                                @if($data[$antMiner->id])
-                                    @foreach($data[$antMiner->id]['chains'] as $chain_index => $chain_data)
-                                        <a class="btn btn-default btn-xs freq">B{{$loop->index + 1}}: {{intval($chain_data['brd_freq'])}}</a>
-                                        <div class="progress vertical progress-xxs">
-                                            <div class="progress-bar progress-bar-info progress-bar-striped"
-                                                 role="progressbar"
-                                                 aria-valuenow="{{intval($chain_data['brd_freq'])}}"
-                                                 aria-valuemin="100" aria-valuemax="1000"
-                                                 style="height: {{round(intval($chain_data['brd_freq']) * 100 / (1000 - 100))}}%">
-                                            </div>
-                                        </div>
-                                    @endforeach
-                                @endif
-                            </td>
-                        @else
-                            <td colspan="5" class="text-center alert-danger">ERROR: miner is offline or unable to connect.</td>
-                        @endif
+                            @endif
+                        </td>
+                    @else
+                        <td colspan="5" class="text-center alert-danger">ERROR: miner is offline or unable to connect.</td>
+                    @endif
 
 
                         <td class="text-right">
