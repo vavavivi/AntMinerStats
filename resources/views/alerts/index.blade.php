f5588f9fbd7189198a28ac27ce07f75dfe4f6658
hippie <v.v.gura@gmail.com>
2017-09-06 10:27:26 +0300

    alerts

--- resources/views/alerts/index.blade.php
+++ resources/views/alerts/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <h1 class="pull-left">Alerts</h1>
-
-    </section>
     <div class="content">
         <div class="clearfix"></div>
         @include('adminlte-templates::common.errors')
@@ -13,18 +9,32 @@
         <div class="row">
                 <div class="col-md-12">
                     <div class="box box-primary">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Alerts</h3>
+
+                            <!-- /.box-tools -->
+                        </div>
                         <!-- /.box-header -->
                         <div class="box-body no-padding">
+                            <div class="mailbox-controls">
+                                <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-check-square-o"></i></button>
+                                <button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
+                            </div>
                             <div class="table-responsive mailbox-messages">
                                 <table class="table table-hover table-striped">
                                     <tbody>
+
                                     @foreach($alerts as $alert)
                                         <tr>
-                                            <td class="mailbox-name"><a href="{{route('alerts.read',$alert->id)}}">{{$alert->antMiner->title}}</a></td>
+                                            <td>
+                                                <input type="checkbox">
+                                            </td>
+                                            <td class="mailbox-name">
+                                                <a href="{{route('alerts.read',$alert->id)}}">{{$alert->antMiner->title}}</a>
+                                            </td>
                                             <td class="mailbox-subject">
                                                 {!! $alert->status == 'new' ? '<b>' : '' !!}{{$alert->subject}}{!!$alert->status == 'new' ? '</b>' : '' !!} - {!! $alert->body !!}
                                             </td>
-                                            <td class="mailbox-attachment"></td>
                                             <td class="mailbox-date">{{$alert->created_at->diffForHumans()}}</td>
                                         </tr>
                                     @endforeach
@@ -35,6 +45,21 @@
                             <!-- /.mail-box-messages -->
                         </div>
                         <!-- /.box-body -->
+                        <div class="box-footer no-padding">
+                            <div class="mailbox-controls">
+                                <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i></button>
+                                <button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
+                                <div class="pull-right">
+                                    1-50/200
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
+                                        <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
+                                    </div>
+                                    <!-- /.btn-group -->
+                                </div>
+                                <!-- /.pull-right -->
+                            </div>
+                        </div>
                     </div>
                     <!-- /. box -->
                 </div>
@@ -43,3 +68,25 @@
     </div>
 @endsection
 
+@section('scripts')
+    <script>
+        $(".checkbox-toggle").click(function () {
+            var clicks = $(this).data('clicks');
+
+            if (clicks == null){
+                var clicks = true;
+            }
+
+            console.log(clicks);
+
+            if (clicks) {
+                //Uncheck all checkboxes
+                $(".mailbox-messages input[type='checkbox']").prop('checked', true);
+            } else {
+                //Check all checkboxes
+                $(".mailbox-messages input[type='checkbox']").prop('checked', false);
+            }
+            $(this).data("clicks", !clicks);
+        });
+    </script>
+@endsection
