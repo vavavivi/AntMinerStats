72eadfc48132300b8756272bcc806901b4bd92d9
torbik (Alexander Dzuba) <a.y.dzuba@gmail.com>
2017-09-07 14:29:20 +0300

    New logo New colors

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,49 +4,52 @@
     <div class="login-box">
         <div class="login-box-body">
             <div class="login-logo">
-                <img src="/images/antstats-logo-bw.png">
-                <br>
-                <a href="{{ url('/home') }}">{!! env('APP_FULL_NAME') !!}</a>
+                <img src="/images/antstats-logo-bw.png"><br>
+                <a href="{{ url('/home') }}">{!! env('APP_FULL_NAME') !!}</a><br>
+
             </div>
+
+            <p>AntMiners monitoring</p>
 
             <form method="post" action="{{ url('/login') }}">
                 {!! csrf_field() !!}
 
                 <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
-                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="user@domain.com">
+                    <input type="email" class="form-control input-sm" name="email" value="{{ old('email') }}" placeholder="user@domain.com">
                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                     @if ($errors->has('email'))
-                        <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
                     @endif
                 </div>
 
                 <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input type="password" class="form-control" placeholder="Password" name="password">
+                    <input type="password" class="form-control input-sm" placeholder="Password" name="password">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                     @if ($errors->has('password'))
-                        <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
                     @endif
-
                 </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember"> Remember me
+                    </label>
+                </div>
+
                 <div class="row">
                     <div class="col-xs-12">
-                        <div class="checkbox pull-left">
-                            <label>
-                                <input type="checkbox" name="remember"> Remember me
-                            </label>
-                        </div>
-                        <a class="btn btn-sm btn-default btn-flat pull-right" href="{{ url('/password/reset') }}" style="margin-left: 5px; margin-top: 5px;">Forgot password</a>
-                        <button type="submit" class="btn btn-sm btn-primary btn-flat  pull-right" style="margin-left: 5px;  margin-top: 5px;">Login</button>
+                        <button type="submit" class="btn btn-sm btn-primary btn-flat" style="margin-top: 5px;"><i class="fa fa-sign-in"></i> Login</button>
+                        <a class="btn btn-sm btn-warning btn-flat" href="{{ url('/password/reset') }}" style="margin-left: 5px; margin-top: 5px;">Forgot password</a>
+                        <a class="btn btn-sm btn-danger btn-flat pull-right" href="{{ url('/register') }}" style="margin-left: 5px; margin-top: 5px;"><i class="fa fa-user-plus"></i> Register</a>
                     </div>
                 </div>
             </form>
             <div class="row">
                 <div class="col-xs-12">
-                    <a class="pull-right" href="{{ url('/register') }}" style="margin-left: 5px; margin-top: 5px;">Dont have account ? Register!</a>
+
                 </div>
             </div>
 
