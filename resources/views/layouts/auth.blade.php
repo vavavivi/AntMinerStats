abfe1f606fbe4aafb2699527a39da2e72e0eedc9
torbik (Alexander Dzuba) <a.y.dzuba@gmail.com>
2017-09-10 11:51:25 +0300

    FIX OpenGraph&SEO

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,22 +2,25 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Authorization - {!! env('APP_NAME') !!}</title>
 
-    <title>Login - {!! env('APP_NAME') !!}</title>
+    @include('layouts.meta-seo')
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- BASE CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/AdminLTE.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="/css/custom.css?v080917">
 
     <!-- PLUGINS -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
 
-    <!-- CUSTOM -->
-    <link rel="stylesheet" href="/css/custom.css?v080917">
+@yield('css')
 @yield('css')
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
---
+++ resources/views/layouts/meta-seo.blade.php
@@ -0,0 +1,19 @@
+<meta name="theme-color" content="#437096">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+
+<!-- OPENGRAPH -->
+<meta property="og:site_name"   content="{!! env('APP_FULL_NAME') !!}">
+<meta property="og:title"       content="@yield('title'){!! env('APP_NAME') !!}">
+<meta property="og:description" content="Real time monitoring system for Bitmain ANTMINERs. Support for S7, S9 & T9 ASICs. Alerts & notifications via telegram bot. Free for home use.">
+<meta property="og:image"       content="/images/share.png">
+<meta property="og:locale"      content="en_GB">
+<meta property="og:type"        content="article">
+<meta property="og:url"         content="https://antstats.info">
+
+<!-- FAVICONS -->
+<link rel="icon" href="/images/icons/favicon-64.png" type="image/x-icon" />
+<link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/images/icons/favicon-180.png">
+<link rel="manifest" href="/images/icons/manifest.json">
