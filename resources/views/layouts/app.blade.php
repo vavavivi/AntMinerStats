a61413b22746aedb6ef056aff8a5e06e26d5e75f
torbik (Alexander Dzuba) <a.y.dzuba@gmail.com>
2017-09-10 09:11:42 +0300

    FAVICONS + SEO + OpenGraph

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,15 +3,29 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <meta name="theme-color" content="#437096">
+
+    <title>@yield('title') {!! env('APP_NAME') !!}</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title') {!! env('APP_NAME') !!}</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- SEO -->
+    <meta property="og:site_name"   content="{!! env('APP_FULL_NAME') !!}">
+    <meta property="og:title"       content="@yield('title') {!! env('APP_NAME') !!}">
+    <meta property="og:description" content="Real time monitoring system for Bitmain ANTMINERs. Support for S7, S9 & T9 ASICs. Alerts & notifications via telegram bot. Free for home use.">
+    <meta property="og:image"       content="/images/share.png">
+    <meta property="og:locale"      content="en_GB">
+    <meta property="og:type"        content="article">
+    <meta property="og:url"         content="https://antstats.info">
 
-    <link rel="icon" href="/images/favicon.png" type="image/x-icon" />
+    <!-- FAVICONS -->
+    <link rel="icon" href="/images/icons/favicon.png" type="image/x-icon" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/favicon-apple-180.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16.png">
+    <link rel="manifest" href="/images/icons/manifest.json">
 
     <!-- BASE CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
