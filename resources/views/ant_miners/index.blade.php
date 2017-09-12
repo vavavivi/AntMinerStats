175ada9a1b793137639ad33ade45cff9353be4a7
torbik (Alexander Dzuba) <a.y.dzuba@gmail.com>
2017-09-12 10:45:10 +0300

    Update table skin

--- resources/views/ant_miners/index.blade.php
+++ resources/views/ant_miners/index.blade.php
@@ -7,24 +7,24 @@
        {{-- --}} <h1 class="pull-left">AntMiners</h1>
         <h1 class="pull-right">
 
-           <div class="btn-group">
-
-               {!! Form::open(['route' => ['antMiners.force'], 'method' => 'post']) !!}
-
-               <button class="btn btn-sm btn-primary">
-                   <i class="fa fa-retweet"></i> Force reload
-               </button>
-               {!! Form::close() !!}
-
+           <div class="btn-inline">
+               <!-- BUTTON: ADD -->
                <a class="btn btn-sm btn-success" href="{!! route('antMiners.create') !!}"><span class="hidden-xs">Add miner </span> <i class="fa fa-plus"></i> </a>
 
-                {!! Form::open(['route' => ['antMiners.view'], 'method' => 'post']) !!}
+               <!-- BUTTON: UPDATE -->
+               {!! Form::open(['route' => ['antMiners.force'], 'method' => 'post']) !!}
+               <button class="btn btn-sm btn-warning"><span class="hidden-xs">Update</span>  <i class="fa fa-refresh"></i></button>
+               {!! Form::close() !!}
+
+               <!-- BUTTON: TABLE VIEW -->
+               {!! Form::open(['route' => ['antMiners.view'], 'method' => 'post']) !!}
                 <input type="hidden" name="view" value="table">
                 <button class="btn btn-sm {{session()->get('miners_view','table') == 'table' ? 'btn-primary' : 'btn-white'}}">
                     <i class="fa fa-table"></i>
                 </button>
                 {!! Form::close() !!}
 
+                <!-- BUTTON: WIDGET VIEW -->
                 {!! Form::open(['route' => ['antMiners.view'], 'method' => 'post']) !!}
                 <input type="hidden" name="view" value="widget">
                 <button class="btn btn-sm {{session()->get('miners_view') == 'widget' ? 'btn-primary' : 'btn-white'}}">
