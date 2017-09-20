9fc06fed477855527f9a79eda91c837fa6db7ac4
hippie <v.v.gura@gmail.com>
2017-09-20 23:21:18 +0300

    fix

--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -10,16 +10,15 @@
             <div class="box-body">
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="box box-default">
+                        <div class="box box-default collapsed-box">
                             <div class="box-header with-border">
-                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?
-                                </h3>
+                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?</h3>
                                 <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
                                     </button>
                                 </div>
                             </div>
-                            <div class="box-body" style="">
+                            <div class="box-body">
                                 All latest firmware provides read-only access by default.
                                 That means, that we can read all data from your hardware, but we cannot write or change any of your settings.
                                 However, we have optional functionality of remote restart miners via API.
@@ -29,16 +28,15 @@
                         </div>
                     </div>
                     <div class="col-md-12">
-                        <div class="box box-default">
+                        <div class="box box-default collapsed-box">
                             <div class="box-header with-border">
-                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?
-                                </h3>
+                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?</h3>
                                 <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
                                     </button>
                                 </div>
                             </div>
-                            <div class="box-body" style="">
+                            <div class="box-body">
                                 All latest firmware provides read-only access by default.
                                 That means, that we can read all data from your hardware, but we cannot write or change any of your settings.
                                 However, we have optional functionality of remote restart miners via API.
@@ -48,16 +46,69 @@
                         </div>
                     </div>
                     <div class="col-md-12">
-                        <div class="box box-default">
+                        <div class="box box-default collapsed-box">
                             <div class="box-header with-border">
-                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?
-                                </h3>
+                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?</h3>
                                 <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
                                     </button>
                                 </div>
                             </div>
-                            <div class="box-body" style="">
+                            <div class="box-body">
+                                All latest firmware provides read-only access by default.
+                                That means, that we can read all data from your hardware, but we cannot write or change any of your settings.
+                                However, we have optional functionality of remote restart miners via API.
+                                And to enable this functionality you have to make some changes to cgminer.conf via SSH.
+                                To make it safer, you can setup your antminer to allow write acceess ONLY from IP address of our service.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="box box-default collapsed-box">
+                            <div class="box-header with-border">
+                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?</h3>
+                                <div class="box-tools pull-right">
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="box-body">
+                                All latest firmware provides read-only access by default.
+                                That means, that we can read all data from your hardware, but we cannot write or change any of your settings.
+                                However, we have optional functionality of remote restart miners via API.
+                                And to enable this functionality you have to make some changes to cgminer.conf via SSH.
+                                To make it safer, you can setup your antminer to allow write acceess ONLY from IP address of our service.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="box box-default collapsed-box">
+                            <div class="box-header with-border">
+                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?</h3>
+                                <div class="box-tools pull-right">
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="box-body">
+                                All latest firmware provides read-only access by default.
+                                That means, that we can read all data from your hardware, but we cannot write or change any of your settings.
+                                However, we have optional functionality of remote restart miners via API.
+                                And to enable this functionality you have to make some changes to cgminer.conf via SSH.
+                                To make it safer, you can setup your antminer to allow write acceess ONLY from IP address of our service.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="box box-default collapsed-box">
+                            <div class="box-header with-border">
+                                <h3 class="box-title">Is it safe to grant access to API of your antminer to our monitoring service?</h3>
+                                <div class="box-tools pull-right">
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="box-body">
                                 All latest firmware provides read-only access by default.
                                 That means, that we can read all data from your hardware, but we cannot write or change any of your settings.
                                 However, we have optional functionality of remote restart miners via API.
@@ -72,3 +123,6 @@
     </div>
 @endsection
 
+
+@section('scripts')
+@endsection
