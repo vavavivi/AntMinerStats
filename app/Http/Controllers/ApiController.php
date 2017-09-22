e445140bbed9cbe360c530f6f478561a171a3051
hippie <v.v.gura@gmail.com>
2017-09-22 15:42:46 +0300

    ID Command

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Telegram\Commands\StartCommand;
 use Illuminate\Http\Request;
 use Telegram;
 
@@ -18,23 +17,6 @@ class ApiController extends Controller
     public function webhook(Request $request)
     {
 	    $update = Telegram::commandsHandler(true);
-
-	    $message = $update['message'];
-
-
-	    if(false)
-	    {
-		    $chat_id = $update['message']['chat']['id'];
-
-		    $msg = 'Welcome to antMiner notify service. Your chat ID is: <strong>'.$chat_id.'</strong>. 
-			Please fill chat id in your <a href="'.route('profile').'">profile</a>.
-    	';
-		    Telegram::sendMessage([
-			    'chat_id' => $chat_id,
-			    'text' => json_encode($update),
-			    'parse_mode' =>'HTML'
-		    ]);
-	    }
 
 	    return 'ok';
     }
