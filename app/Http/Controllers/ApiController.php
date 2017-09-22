c6172436048a476c704788a5ab93937fb94d9d5a
hippie <v.v.gura@gmail.com>
2017-09-22 14:39:33 +0300

    move to Commands system

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,20 +13,13 @@ class ApiController extends Controller
 	    $response = Telegram::setWebhook(['url' => route('webHook')]);
 
 	    return $response;
-
     }
 
     public function webhook(Request $request)
     {
-
 	    $update = Telegram::commandsHandler(true);
 
-	    // Commands handler method returns an Update object.
-	    // So you can further process $update object
-	    // to however you want.
-
 	    return 'ok';
-
     }
 
 }
