30d1fc2fa0a1e4c75406372c38a2dd70234a1f02
hippie <v.v.gura@gmail.com>
2017-09-22 15:34:32 +0300

    ID Command

--- app/Telegram/Commands/MInersCommand.php
+++ app/Telegram/Commands/MInersCommand.php
@@ -3,7 +3,6 @@
 namespace App\Telegram\Commands;
 
 use Telegram;
-use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
 
 class MinersCommand extends Command
@@ -25,24 +24,6 @@ class MinersCommand extends Command
 	{
 		$chat_id = $this->update->getMessage()->getChat()->getId();
 
-		$keyboard = [
-			['7', '8', '9'],
-			['4', '5', '6'],
-			['1', '2', '3'],
-			['0']
-		];
-
-		$reply_markup = Telegram::replyKeyboardMarkup([
-			'keyboard' => $keyboard,
-			'resize_keyboard' => true,
-			'one_time_keyboard' => true
-		]);
-
-		Telegram::sendMessage([
-			'chat_id' => $chat_id,
-			'text' => 'Hello World',
-			'reply_markup' => $reply_markup
-		]);
 
 	}
 }
