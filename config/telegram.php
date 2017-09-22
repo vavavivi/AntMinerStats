f49bb722b12b136038b77b9ce591ce513c567201
hippie <v.v.gura@gmail.com>
2017-09-22 15:43:31 +0300

    ID Command

--- config/telegram.php
+++ config/telegram.php
@@ -56,8 +56,6 @@ return [
 	|
 	*/
 	'commands' => [
-		\App\Telegram\Commands\StartCommand::class,
-		\App\Telegram\Commands\IdCommand::class,
 		\App\Telegram\Commands\MinerCommand::class,
 		Telegram\Bot\Commands\HelpCommand::class,
 	],
