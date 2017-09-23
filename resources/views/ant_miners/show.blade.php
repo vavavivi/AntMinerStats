8ceb9ac2e0a9f1ff4e573f981f407cff41a717e5
torbik (Alexander Dzuba) <a.y.dzuba@gmail.com>
2017-09-23 15:30:31 +0300

    Update: miner info

--- resources/views/ant_miners/show.blade.php
+++ resources/views/ant_miners/show.blade.php
@@ -6,11 +6,11 @@
     <section class="content-header">
         <h1 class="pull-left">{!! $antMiner->title !!} <small>{{$antMiner->type == 'bmminer' ? 'Antminer S9/T9' : 'Antminer S7'}}</small></h1>
         <div class="btn-inline hidden-xs">
-            <a href="{!! route('antMiners.index') !!}" class="btn btn-sm btn-info pull-right">&larr; Back</a>
+            <a href="{!! route('antMiners.index') !!}" class="btn btn-sm btn-primary">&larr; Back</a>
             @if($antMiner->url)
-                <a href="{{$antMiner->url}}" class="btn btn-sm btn-success pull-right" target="_blank"><i class="glyphicon glyphicon-share"></i> Manage</a>
+                <a href="{{$antMiner->url}}" class="btn btn-sm btn-success" target="_blank"><i class="glyphicon glyphicon-share"></i> Manage</a>
             @else
-                <a href="#" class="btn btn-sm btn-success disabled pull-right" target="_blank"><i class="glyphicon glyphicon-share"></i> Manage</a>
+                <a href="#" class="btn btn-sm btn-success disabled" target="_blank"><i class="glyphicon glyphicon-share"></i> Manage</a>
             @endif
             <a href="{!! route('antMiners.edit', [$antMiner->id]) !!}" class="btn btn-sm  btn-warning pull-right"><i class="fa fa-cog"></i> Configuration</a>
         </div>
@@ -20,7 +20,7 @@
 
         @include('ant_miners.show_fields')
 
-        <a href="{!! route('antMiners.index') !!}" class="btn btn-info">&larr; Back</a>
+        <a href="{!! route('antMiners.index') !!}" class="btn btn-primary">&larr; Back</a>
         @if($antMiner->url)
             <a href="{{$antMiner->url}}" class='btn btn-success' target="_blank"><i class="glyphicon glyphicon-share"></i> Manage</a>
         @else
